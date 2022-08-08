635d69398aa34f9ab832aaf47c56b7d16088c6ab
Jorge Vazquez <jorgevazquez@wolfsellers.com>
2022-08-08 16:56:37 -0500

    header mobiles/desk

--- components/Header/header.js
+++ components/Header/header.js
@@ -1,17 +1,67 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import styles from './header.module.css';
-import { IconLocation, LogoAmex } from '../Icons/icons';
+import {
+    IconAmexSegurosBlue,
+    IconAmexSegurosWhite,
+    IconHamburguer,
+    IconLocation, IconX,
+    LogoAmex
+} from '../Icons/icons';
+import {useWindowWidth} from "@react-hook/window-size";
 
 const Header = () => {
+
+    const onlyWidth = useWindowWidth()
+    const isMobile = onlyWidth<=768;
+
+    const [active, setActive] = useState(false)
+
+    const handleClick = useCallback(()=> {
+        setActive(active=>!active)
+    },[setActive]);
+
+    const changeColor =  active? styles.active:styles.root;
+
+    const changeIcon = active?styles.iconCloseX: styles.iconHamburguer;
+
     return (
-        <div className={styles.root}>
-            <span className={styles.contentLogo}>
-                <LogoAmex className={styles.logoAmex} />
-            </span>
-            <a className={styles.contentLocation} href={'#'}>
-                <IconLocation />
-                <span>Mapa</span>
-            </a>
+        <div className={changeColor }>
+            {isMobile?
+                <>
+                    {active?
+                        <div className={styles.backgroundBlack}>
+                            <div className={styles.links}>
+                                <a className={styles.contentLocation} href={'#'}>
+                                    <IconLocation />
+                                    <span>Mapa</span>
+                                </a>
+                            </div>
+                        </div>:null
+                    }
+                    <div className={styles.containerIconsMobile}>
+                        <div className={changeIcon} onClick={handleClick}>
+                            {active ? <IconX className={styles.iconX} viewBox={'0 0 24 24'}/> :
+                                <IconHamburguer/>
+                            }
+                        </div>
+                        <div>
+                            {active? <IconAmexSegurosWhite/>:
+                                <IconAmexSegurosBlue/>
+                            }
+                        </div>
+                    </div>
+                </>
+                :
+                <>
+                    <div className={styles.contentLogo}>
+                        <LogoAmex className={styles.logoAmex} />
+                    </div>
+                    <a className={styles.contentLocation} href={'#'}>
+                        <IconLocation />
+                        <span>Mapa</span>
+                    </a>
+                </>
+            }
         </div>
     );
 };

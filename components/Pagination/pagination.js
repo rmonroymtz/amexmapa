77f02e88bf9668af4c46fb9d7fbdb93c42c323de
Jorge Vazquez <jorgevazquez@wolfsellers.com>
2022-08-11 14:44:00 -0500

    styles pagination

--- components/Pagination/pagination.js
+++ components/Pagination/pagination.js
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import {IconChevronLeft} from "../Icons/icons";
 
 const Pagination = ({
     styles,
@@ -9,8 +10,14 @@ const Pagination = ({
     handleChangePage = () => {}
 }) => {
     const buttons = Array.from(Array(totalPages).keys()).map((index) => {
+
+        const active = index===currentPage -1
+
+        const changeBtn = active? styles.number:styles.numberHidden
+
         return (
             <button
+                className={changeBtn}
                 key={`button-${index}`}
                 disabled={index === currentPage - 1}
                 onClick={() => handleChangePage(index + 1)}
@@ -22,15 +29,22 @@ const Pagination = ({
 
     return (
         <div className={styles.containerPagination}>
-            <button onClick={handlePrevPage} disabled={currentPage === 1}>
-                Previous
+            <button
+                className={styles.btnPrev}
+                onClick={handlePrevPage}
+                disabled={currentPage === 1}
+            >
+                <IconChevronLeft className={styles.iconLeft}/>
+                <span className={styles.labelPagination}>Previous</span>
             </button>
             {buttons}
             <button
+                className={styles.btnPrev}
                 onClick={handleNextPage}
                 disabled={currentPage === totalPages}
             >
-                Next
+                <span className={styles.labelPagination}>Next</span>
+                <IconChevronLeft className={styles.iconRight}/>
             </button>
         </div>
     );

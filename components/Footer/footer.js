847a13135d22873519ece7e67c1ed3f08c04769f
Ricardo Monroy <ricardomonroy@wolfsellers.com>
2022-08-12 13:58:33 -0500

    fix: footer on local

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -1,13 +1,17 @@
 import classes from './footer.module.css';
-const Footer = (props) => {
-    const { footer: component, error } = props;
+import { useFooter } from './useFooter';
+const Footer = () => {
+
+    const { footer, error } = useFooter();
+
     if (error) return 'Error load footer';
 
-    if (!component) return null;
+    if (!footer) return null;
+
     return (
         <div
             className={classes.footer}
-            dangerouslySetInnerHTML={{ __html: component }}
+            dangerouslySetInnerHTML={{ __html: footer }}
         />
     );
 };
